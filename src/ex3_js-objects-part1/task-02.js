4b4d4c960139db34d45e15cd998b30167bbbf47b
Alexander <45360388+altitlin@users.noreply.github.com>
2019-03-26 18:06:03 +0300

    Delete task-02.js
    deleted file not related to ex 4

--- src/ex3_js-objects-part1/task-02.js
+++
@@ -1,7 +0,0 @@
-function getObjShowKeyAndValue(obj) {
-    for (var key in obj) {
-        console.log('Ключ объекта: ' + key + ' значение свойства объекта: ' + obj[key]);
-    }
-}
-
-module.exports = getObjShowKeyAndValue
