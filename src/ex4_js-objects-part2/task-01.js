7898978c06c25ab5026017c6d6c6e96d2c70afde
Тестируемый(ая) <t@local.rsu.edu.ru>
2019-04-18 20:32:56 +0300

    Task-01 fixed. Return property of object

--- src/ex4_js-objects-part2/task-01.js
+++ src/ex4_js-objects-part2/task-01.js
@@ -4,10 +4,11 @@ function isPropertyInPrototype(property, obj) {
     for (var key in obj) {
         if (key === property) {
             if (!obj.hasOwnPropery(key)) continue;
+            return obj[property];
         }
     }
 
-    return;
+    return undefined;
 }
 
 module.exports = isPropertyInPrototype
