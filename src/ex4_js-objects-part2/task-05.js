f4739f1a5bacd0b30e1b17a1d24421ae21ac57da
Тестируемый(ая) <t@local.rsu.edu.ru>
2019-04-18 20:34:19 +0300

    Task-05 fixed. Removed variable

--- src/ex4_js-objects-part2/task-05.js
+++ src/ex4_js-objects-part2/task-05.js
@@ -1,7 +1,5 @@
 function searchSubString(str, substr) {
-    var temp = str.indexOf(substr);
-
-    if (temp === -1) return false;
+    if (str.indexOf(substr) === -1) return false;
 
     return true;
 }
