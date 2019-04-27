d6778589533176dc5dae771a73cfee479f20a407
Alexander Titov <titov.alex627@gmail.com>
2019-04-27 13:51:32 +0300

    Created a calculator with a similar practical work "Functions in JavaScript" API, but with some modifications

---
+++ src/ex14_js-context/task-01.js
@@ -0,0 +1,67 @@
+var Calculator = function(){
+    this.currentState = 0;
+
+    this.add = function(value) {
+        if (typeof value !== 'undefined') {
+            this.currentState += value;
+        }
+
+        return this;
+    }
+
+    this.subtract = function(value) {
+        if(typeof value !== 'undefined') {
+            this.currentState -= value;
+        }
+
+        return this;
+    }
+
+    this.multiply = function(value) {
+        if(typeof value !== 'undefined') {
+            this.currentState *= value;
+        }
+
+        return this;
+    }
+
+    this.divide = function(value) { 
+        if(typeof value !== 'undefined') {
+            this.currentState /= value;
+        }
+
+        return this;
+    }
+
+    this.reset = function() {
+        this.currentState = 0;
+        return this;
+    }
+
+    this.getResult = function() {
+        return this.currentState;
+    }
+
+    this.setState = function(value) {
+        if(typeof value !== 'undefined') {
+            this.currentState = value;
+        }
+
+        return this;
+    }
+
+    this.fetchData = function(callback) {
+        var cb = callback.bind(this);
+
+        setTimeout(function(){
+            var serverState = 500;
+            cb(serverState);
+        }, 1000);
+
+        return this;
+    }
+}
+
+var Calculator = new Calculator();
+
+module.exports = Calculator;
