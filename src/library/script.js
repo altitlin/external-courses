17c0cfe5f725e9e9390ecb55e5bf05f30aed79d9
Alaxander <alaxander@gmail.com>
2019-05-03 14:45:18 +0300

    Created a callApi function that processes the request; changed the name of the showBooks function parameter; pressed the footer to the bottom of the page; used a forEach loop; created createBook function.

--- src/library/script.js
+++ src/library/script.js
@@ -1,45 +1,57 @@
 var requestURL = 'https://rsu-library-api.herokuapp.com/books';
 var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
 
-request.onload = function() {
-    var books = request.response;  
-    showBooks(books);
+function callApi(url, callback, objSetting) {
+    var requestURL = url;
+    var request = objSetting;
+
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        var books = request.response;
+        callback(books);
+    }
 }
 
-function showBooks(jsonObj) {
+callApi(requestURL, showBooks, request);
+
+function createBook(item) {
+    var article = document.createElement('article');
+    var figure = document.createElement('figure');
+    var img = document.createElement('img');
+    var figcaption = document.createElement('figcaption');
+    var span = document.createElement('span');
+    var div = document.createElement('div');
+    div.className = 'rating';
+
+    img.src = item.image_url;
+    img.alt = item.title;
+    figcaption.textContent = item.title;
+    span.textContent = 'by ' + item.author.firstName + ' ' + item.author.lastName;
+
+    article.appendChild(figure);
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    article.appendChild(span);
+    article.appendChild(div);
+
+    var rating = item.rating;
+
+    for (let j = 0; j < rating; j++) {
+        var star = document.createElement('i');
+        star.className = 'fas fa-star';
+        div.appendChild(star);            
+    }
+
+    return article;
+}
+
+function showBooks(booksArray) {
     var section = document.querySelector('section');
 
-    for (let i = 0; i < jsonObj.length; i++) {
-        var article = document.createElement('article');
-        var figure = document.createElement('figure');
-        var img = document.createElement('img');
-        var figcaption = document.createElement('figcaption');
-        var span = document.createElement('span');
-        var div = document.createElement('div');
-        div.className = 'rating';
-
-        img.src = jsonObj[i].image_url;
-        img.alt = jsonObj[i].title;
-        figcaption.textContent = jsonObj[i].title;
-        span.textContent = 'by ' + jsonObj[i].author.firstName + ' ' + jsonObj[i].author.lastName;
-
-        article.appendChild(figure);
-        figure.appendChild(img);
-        figure.appendChild(figcaption);
-        article.appendChild(span);
-        article.appendChild(div);
-
-        var rating = jsonObj[i].rating;
-
-        for (let j = 0; j < rating; j++) {
-            var star = document.createElement('i');
-            star.className = 'fas fa-star';
-            div.appendChild(star);            
-        }
-
-        section.appendChild(article);
-    }
+    booksArray.forEach(function(item) {
+        var itemBook = createBook(item);
+        section.appendChild(itemBook);
+    });
 }
